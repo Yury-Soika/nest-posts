2f4efc610a66057a1ed4e12684dc71a0b553e69b
Yury-Soika <puma10009@gmail.com>
2024-01-06 23:03:27 +0100

    Added docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,9 +15,9 @@ import { Post } from './posts/posts.model';
     SequelizeModule.forRoot({
       dialect: 'postgres',
       host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRESS_PORT),
+      port: Number(process.env.POSTGRES_PORT),
       username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRESS_PASSWORD,
+      password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
       models: [User, Post],
       autoLoadModels: true,
